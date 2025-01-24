11750a450f0c28647a7388106615a2ec4de09efa
SenseiJM <loudeche.jean-michel@hotmail.fr>
2025-01-24 11:01:43 +0100

    Ajout exercices 1.1 et 1.2 python

---
+++ Python/Partie 1/Exercice1.1.py
@@ -0,0 +1,10 @@
+# Exercice 1.1
+
+#Chaine et Caractere = str
+#Nombre = int
+
+nombre = 42
+texte = "test"
+caractere = 'f'
+
+print(nombre, texte, caractere)
---
+++ Python/Partie 1/Exercice1.2.py
@@ -0,0 +1,5 @@
+#Exercice1.2
+
+nombre1 = 16
+nombre2 = 22
+print(nombre1 + nombre2)
