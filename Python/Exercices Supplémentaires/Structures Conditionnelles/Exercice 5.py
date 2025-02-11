cea02e1f6aa7a47a07f9b3bfabcdb1d2108e6795
SenseiJM <loudeche.jean-michel@hotmail.fr>
2025-02-11 12:08:09 +0100

    Ajout exo supp 5 conditions

---
+++ Python/Exercices Supplémentaires/Structures Conditionnelles/Exercice 5.py
@@ -0,0 +1,9 @@
+#Exercice 5 - Consonne ou voyelle
+
+print("Veuillez entrer une lettre : ")
+lettre = input()
+
+if (lettre == 'a') or (lettre == 'e') or (lettre == 'i') or (lettre == 'o') or (lettre == 'u') or (lettre == 'y') :
+    print("Ceci est une voyelle")
+else :
+    print("Ceci est une consonne")
