9876a5931c1cbf87070989a9c21d671d2399662d
SenseiJM <loudeche.jean-michel@hotmail.fr>
2025-02-11 10:28:44 +0100

    Ajout Exercices supplémentaires conditionnelles 1 et 2

---
+++ Python/Exercices Supplémentaires/Exercice1.py
@@ -0,0 +1,11 @@
+#Exercice 1
+
+print("Veuillez entrer un premier nombre : ")
+nombre1 = int(input())
+print("Veuillez entrer un deuxième nombre : ")
+nombre2 = int(input())
+
+if nombre1 == nombre2 :
+    print("Ces nombres sont égaux !")
+else :
+    print("Ces nombres ne sont pas égaux !")
---
+++ Python/Exercices Supplémentaires/Structures Conditionnelles/Exercice 2.py
@@ -0,0 +1,9 @@
+#Exercice 2 - Nombre pair
+
+print("Veuillez entrer un nombre : ")
+nombre = int(input())
+
+if (nombre % 2) == 0 :
+    print(str(nombre) + " est pair !")
+else :
+    print(str(nombre) + " est impair !")
